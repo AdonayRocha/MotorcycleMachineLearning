2d0d9d24a3c9d09a625c06f02b61f19a23fafd28
adonayrocha <adonay.rodrigues.rocha@gmail.com>
2025-09-25 01:33:33 -0300

    feat: integração com Oracle, contagem única de motocicletas, setup automatizado e dependências atualizadas

--- main.py
+++ main.py
@@ -1,22 +1,53 @@
+from datetime import datetime, timedelta
 from inference import InferencePipeline
 import cv2
+import oracledb
 
-#Trata os resultados
+def save_db(qtd_motos):
+    conn = oracledb.connect(
+        user="RM558782",
+        password="Fiap#2025",
+        dsn="oracle.fiap.com.br:1521/ORCL"
+    )
+    cursor = conn.cursor()
+    agora = datetime.now()
+    data = agora.date()
+    hora = agora.strftime("%H:%M:%S")
+    cursor.execute("""
+        INSERT INTO TB_ECHO_MOTORCYCLE (QTD_MOTOS, HORA, DATA)
+        VALUES (:qtd, :hora, :data)
+    """, qtd=qtd_motos, hora=hora, data=data)
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+motorcycle_ids = [set()]
+last_db_save = [datetime.min]
 def my_sink(result, video_frame):
-    if result.get("output_image"):
+    if result.get("output_image"):      
         cv2.imshow("Imagem com detecções", result["output_image"].numpy_image)
         cv2.waitKey(1)
 
-    motocicletas = []
     for p in result["predictions"]:
+        print("Predição completa:", p)
+        detection_id = None
         if isinstance(p, dict):
-            if p.get("class_name", "").lower() == "motocicleta":
-                motocicletas.append(p)
+            if p.get("class_name", "").lower() == "motorcycle":
+                detection_id = p.get("detection_id")
         elif isinstance(p, tuple):
-            if len(p) > 0 and str(p[0]).lower() == "motocicleta":
-                motocicletas.append(p)
+            if len(p) > 5 and p[5].get("class_name", "").lower() == "motorcycle":
+                detection_id = p[5].get("detection_id")
+        if detection_id:
+            motorcycle_ids[0].add(detection_id)
 
-    print(f"Motocicletas detectadas: {len(motocicletas)}")
+    agora = datetime.now()
+    if (agora - last_db_save[0]).total_seconds() >= 0.01:
+        qtd_motos = len(motorcycle_ids[0])
+        print(f"Motocicletas únicas detectadas no período: {qtd_motos}")
+        save_db(qtd_motos)
+        motorcycle_ids[0].clear()
+        last_db_save[0] = agora
+    return []
 
 
 # Câmera/webcam
@@ -48,7 +79,7 @@ def p_local(api_key, workspace_name, workflow_id, video_path, max_fps=15):
 
 
 # Configuração da API 
-api_key = "secret"
+api_key = "zl2QCx0S5KqmkOzxvjJs"
 workspace_name = "safeyard"
 workflow_id = "detect-count-and-visualize-3"
 
@@ -59,3 +90,4 @@ workflow_id = "detect-count-and-visualize-3"
 
 # Camera
 p_camera(api_key, workspace_name, workflow_id, camera_id=0)
+
